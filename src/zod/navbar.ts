789355f8b90c84e4e86c0c08a435c56e9297c2c7
Md Aminul Islam <mdaminulislam@MacBook.local>
2026-05-27 09:27:04 +0600

    Navbar item added seo input delete

--- src/zod/navbar.ts
+++ src/zod/navbar.ts
@@ -4,9 +4,6 @@ export const navSubItemSchema = z.object({
   title: z.string().min(1, 'Title is required'),
   href: z.string().optional().or(z.literal('')),
   items: z.union([z.string(), z.array(z.string())]),
-  seoTitle: z.string().optional(),
-  seoDescription: z.string().optional(),
-  seoKeywords: z.string().optional(),
 });
 
 export const addNavItemSchema = z.object({
@@ -14,9 +11,6 @@ export const addNavItemSchema = z.object({
   href: z.string().min(1, 'Href is required'),
   subItems: z.array(navSubItemSchema),
   order: z.coerce.number().default(0),
-  seoTitle: z.string().optional(),
-  seoDescription: z.string().optional(),
-  seoKeywords: z.string().optional(),
 });
 
 export const updateNavItemSchema = addNavItemSchema.partial();
