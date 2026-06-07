8dca104b1c3d31c8dba4cb9ea6313b9f3f237052
Md Aminul Islam <mdaminulislam@MacBook.local>
2026-06-07 17:24:20 +0600

    Contact page heading color fix

--- src/components/ui/SectionHeading.tsx
+++ src/components/ui/SectionHeading.tsx
@@ -7,9 +7,11 @@ type HeadingType = {
 const SectionHeading = ({ heading, description, className }: HeadingType) => {
   return (
     <div
-      className={`mx-auto max-w-2xl space-y-3 px-2 py-12 text-center text-white/60 ${className}`}
+      className={`text-muted-foreground mx-auto max-w-2xl space-y-3 px-2 py-12 text-center ${className}`}
     >
-      <p className="text-center text-4xl font-bold">{heading}</p>
+      <p className="text-foreground text-center text-4xl font-bold">
+        {heading}
+      </p>
       <p>{description}</p>
     </div>
   );
